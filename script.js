df6a9a12a979feddc3e5e76f79a642bba329afc0
Zack Huston <zackhuston@Zacks-MacBook-Air.local>
2022-10-09 19:03:27 -0400

    Added API and first function

---
+++ script.js
@@ -0,0 +1,9 @@
+function getApi() {
+    // fetch request gets a list of all the repos for the node.js organization
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={836f663cb0a3ac5683bdc3e1fb6eb7ca}';
+}
+  var myBtn = document.getElementById('myBtn')
+
+  function Btn (event){
+    myBtn.addEventListener('click' console.log('Yo'))
+  }
